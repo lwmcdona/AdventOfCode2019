87cae3c8c1384e2c9e58b2efcc096a639c154278
loganmcd <lwmcdona@ualberta.ca>
2020-09-23 19:55:38 -0600

    Cleaned up solution to Day 10

--- day10/MapSurveyor.java
+++ day10/MapSurveyor.java
@@ -1,5 +1,6 @@
 package day10;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class MapSurveyor {
@@ -8,5 +9,88 @@ public class MapSurveyor {
 
     public MapSurveyor(List<String> map) {
         this.map = map;
+    }
+
+    public int getAsteroidsSeenFromBestMonitoringLocation() {
+        if (this.station == null) {
+            getBestMonitoringLocation();
+        }
+
+        return getStation().getNumAsteroidsSeen();
+    }
+
+    public Station getBestMonitoringLocation() {
+        if (this.station != null) {
+            return getStation();
+        }
+
+        HashMap<Integer, Station> potentialStations = new HashMap<>();
+        int maxDetected = Integer.MIN_VALUE;
+
+        for (int y1 = 0; y1 < this.map.size(); y1++) {
+            String line = this.map.get(y1);
+            for (int x1 = 0; x1 < line.length(); x1++) {
+                if (line.charAt(x1) == '#') {
+                    int startAsteroid = calculateAsteroidNumber(x1, y1);
+                    potentialStations.putIfAbsent(startAsteroid, new Station(startAsteroid));
+                    for (int x2 = x1 + 1; x2 < line.length(); x2++) {
+                        if (line.charAt(x2) == '#') {
+                            int endAsteroid = calculateAsteroidNumber(x2, y1);
+                            Pair<Integer, Integer> direction = getDirection(x1, y1, x2, y1);
+                            addToPotentialStations(potentialStations, startAsteroid, endAsteroid, direction);
+                        }
+                    }
+
+                    for (int y2 = y1 + 1; y2 < this.map.size(); y2++) {
+                        String row = this.map.get(y2);
+                        for (int x2 = 0; x2 < row.length(); x2++) {
+                            if (row.charAt(x2) == '#') {
+                                int endAsteroid = calculateAsteroidNumber(x2, y2);
+                                Pair<Integer, Integer> direction = getDirection(x1, y1, x2, y2);
+                                addToPotentialStations(potentialStations, startAsteroid, endAsteroid, direction);
+                            }
+                        }
+                    }
+
+                    if (maxDetected < potentialStations.get(startAsteroid).getNumAsteroidsSeen()) {
+                        this.station = potentialStations.get(startAsteroid);
+                        maxDetected = this.station.getNumAsteroidsSeen();
+                    }
+                }
+            }
+        }
+        System.out.println("The station chosen is at: " + this.station.getAsteroid());
+        this.station.sort();
+        return getStation();
+    }
+
+    public Station getStation() {
+        return this.station.deepCopy();
+    }
+
+    private int calculateAsteroidNumber(int x, int y) {
+        return x * 100 + y;
+    }
+
+    private Pair<Integer, Integer> getDirection(int x1, int y1, int x2, int y2) {
+        int dx = x2 - x1;
+        int dy = y2 - y1;
+        int gcd = gcdByEuclidsAlgorithm(dx, dy);
+        dx /= gcd;
+        dy /= gcd;
+        return Pair.createNewInstance(dx, dy);
+    }
+
+    private int gcdByEuclidsAlgorithm(int n1, int n2) {
+        if (n2 == 0) {
+            return n1;
+        }
+        return Math.abs(gcdByEuclidsAlgorithm(n2, n1 % n2));
+    }
+
+    private void addToPotentialStations(HashMap<Integer, Station> s, int start, int end, Pair<Integer, Integer> dir) {
+        s.get(start).add(dir, end);
+        s.putIfAbsent(end, new Station(end));
+        s.get(end).add(Pair.createNewInstance(-dir.getFirst(), -dir.getSecond()), start);
     }
 }
--- day10/Station.java
+++ day10/Station.java
@@ -7,112 +7,91 @@ import java.util.LinkedList;
 
 public class Station {
     private int asteroidNum;
-    private HashMap<Double, LinkedList<Integer>> forward;
-    private HashMap<Double, LinkedList<Integer>> backward;
-    private ArrayList<Double> fSortedSlopes;
-    private ArrayList<Double> bSortedSlopes;
-    private boolean useBackward = true;
+    private HashMap<Pair<Integer, Integer>, LinkedList<Integer>> asteroids;
+    private ArrayList<Pair<Integer, Integer>> sortedDirections;
     private int nextVaporized = -1;
 
     public Station(int asteroidNum) {
-        this.forward = new HashMap<>();
-        this.backward = new HashMap<>();
+        this.asteroids = new HashMap<>();
         this.asteroidNum = asteroidNum;
     }
 
-    public void addToForward(double slope, int asteroidNum) {
-        forward.putIfAbsent(slope, new LinkedList<>());
-        forward.get(slope).add(asteroidNum);
+    public void setAsteroids(HashMap<Pair<Integer, Integer>, LinkedList<Integer>> asteroids) {
+        this.asteroids = asteroids;
     }
 
-    public void addToBackward(double slope, int asteroidNum) {
-        backward.putIfAbsent(slope, new LinkedList<>());
-        backward.get(slope).addFirst(asteroidNum);
+    public void setSortedDirections(ArrayList<Pair<Integer, Integer>> sortedDirections) {
+        this.sortedDirections = sortedDirections;
     }
 
-    public int getTotal() {
-        return backward.size() + forward.size();
+    public void setNextVaporized(int nextVaporized) {
+        this.nextVaporized = nextVaporized;
+    }
+
+    public Station deepCopy() {
+        HashMap<Pair<Integer, Integer>, LinkedList<Integer>> asteroidsCopy = new HashMap<>(this.asteroids);
+        ArrayList<Pair<Integer, Integer>> sortedCopy = new ArrayList<>(this.sortedDirections);
+
+        Station toReturn = new Station(this.asteroidNum);
+        toReturn.setNextVaporized(this.nextVaporized);
+        toReturn.setAsteroids(asteroidsCopy);
+        toReturn.setSortedDirections(sortedCopy);
+        return toReturn;
+    }
+
+    public void add(Pair<Integer, Integer> direction, int asteroidNum) {
+        asteroids.putIfAbsent(direction, new LinkedList<>());
+        asteroids.get(direction).add(asteroidNum);
+    }
+
+    public void addFirst(Pair<Integer, Integer> direction, int asteroidNum) {
+        asteroids.putIfAbsent(direction, new LinkedList<>());
+        asteroids.get(direction).addFirst(asteroidNum);
+    }
+
+    public int getNumAsteroidsSeen() {
+        return asteroids.keySet().size();
     }
 
     public void sort() {
-        this.fSortedSlopes = new ArrayList<>(forward.keySet());
-        this.fSortedSlopes.sort(new DirectionComparator());
-        this.bSortedSlopes = new ArrayList<>(backward.keySet());
-        this.bSortedSlopes.sort(new DirectionComparator());
-        this.nextVaporized = findNextVaporized();
-    }
-
-    private int findNextVaporized() {
-        for (int i = 0; i < this.bSortedSlopes.size() - 1; i++) {
-            if (this.bSortedSlopes.get(i) > this.bSortedSlopes.get(i + 1)) {
-                return i;
-            }
-        }
-        this.useBackward = false;
-        return 0;
+        this.sortedDirections = new ArrayList<>(this.asteroids.keySet());
+        this.sortedDirections.sort(new DirectionComparator());
+        this.nextVaporized = 0;
     }
 
     public int vaporize() {
-        int asteroid;
-        if (this.useBackward) {
-            double key = this.bSortedSlopes.get(this.nextVaporized);
-            LinkedList<Integer> asteroids = this.backward.get(key);
-            asteroid = asteroids.pop();
-            if (asteroids.isEmpty()) {
-                this.bSortedSlopes.remove(this.nextVaporized);
-                this.backward.remove(key);
-                this.nextVaporized--;
-            }
-            this.nextVaporized++;
-            if (this.nextVaporized >= this.bSortedSlopes.size()) {
-                this.useBackward = false;
-                this.nextVaporized = 0;
-            }
-        } else {
-            double key = this.fSortedSlopes.get(this.nextVaporized);
-            LinkedList<Integer> asteroids = this.forward.get(key);
-            asteroid = asteroids.pop();
-            if (asteroids.isEmpty()) {
-                this.fSortedSlopes.remove(this.nextVaporized);
-                this.forward.remove(key);
-                this.nextVaporized--;
-            }
-            this.nextVaporized++;
-            if (this.nextVaporized >= this.fSortedSlopes.size()) {
-                this.useBackward = true;
-                this.nextVaporized = 0;
-            }
+        Pair<Integer, Integer> key = this.sortedDirections.get(this.nextVaporized);
+        LinkedList<Integer> asteroids = this.asteroids.get(key);
+        int asteroidNum = asteroids.pop();
+        if (asteroids.isEmpty()) {
+            this.sortedDirections.remove(this.nextVaporized);
+            this.asteroids.remove(key);
+            this.nextVaporized--;
         }
-        return asteroid;
+        this.nextVaporized = (this.nextVaporized + 1) % this.sortedDirections.size();
+        return asteroidNum;
     }
 
     public int vaporize(int num) {
         int asteroidNum = -1;
         for (int i = num; i > 0; i--) {
             asteroidNum = vaporize();
-            System.out.println("The " + (num - i + 1) + "th asteroid to be vaporized is at " + asteroidNum);
+            // System.out.println("The " + (num - i + 1) + "th asteroid to be vaporized is
+            // at " + asteroidNum);
         }
         return asteroidNum;
     }
-
-    // private int calculateCoordinates(int asteroidNum) {
-    // return (asteroidNum % 20 * 100) + (asteroidNum / 20);
-    // }
 
     public int getAsteroid() {
         return this.asteroidNum;
     }
 
-    public class DirectionComparator implements Comparator<Double> {
+    public class DirectionComparator implements Comparator<Pair<Integer, Integer>> {
         @Override
-        public int compare(Double o1, Double o2) {
-            if (o1 == o2) {
-                return 0;
-            } else if ((o1 < 0 && o2 < 0) || (o1 >= 0 && o2 >= 0)) {
-                return o1.compareTo(o2);
-            } else {
-                return o2.compareTo(o1);
-            }
+        public int compare(Pair<Integer, Integer> o1, Pair<Integer, Integer> o2) {
+            Double angle1 = -Math.atan2(o1.getFirst(), o1.getSecond());
+            Double angle2 = -Math.atan2(o2.getFirst(), o2.getSecond());
+            return angle1.compareTo(angle2);
         }
     }
 }
